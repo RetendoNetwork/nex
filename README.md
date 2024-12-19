6bddd109e0417c636435aecb8922dc6dac3b899e
00cedke <mathiskera2509@gmail.com>
2024-12-19 20:35:18 +0100

    [+] Updated server creator.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # nex
-PRUDP/NEX Server Library.
+- PRUDP/NEX Server Library.
+
+## Example Usage
+```Go
+package main
+
+import (
+	"github.com/RetendoNetwork/nex"
+)
+
+func main() {
+	server := nex.NewServer()
+	server.SetPrudpVersion(1)
+	server.SetKeySize(16)
+	server.SetFragmentSize(962)
+	server.SetAccessKey("ridfebb9")
+	server.Listen(":6000")
+}
+```
--- server.go
+++ server.go
@@ -3,23 +3,26 @@ package nex
 import (
 	"fmt"
 	"net"
+	"strconv"
 )
 
 type Server struct {
-	socket    *net.UDPConn
-	accessKey string
+	socket       *net.UDPConn
+	accessKey    string
+	prudpVersion int
+	KeySize      int
+	FragmentSize int
 }
 
-func (server *Server) Listen(address string) {
-
+func (server *Server) Listen(port int) {
 	protocol := "udp"
 
-	udpAddress, _ := net.ResolveUDPAddr(protocol, address)
+	udpAddress, _ := net.ResolveUDPAddr(protocol, ":"+strconv.Itoa(port))
 	socket, _ := net.ListenUDP(protocol, udpAddress)
 
 	server.SetSocket(socket)
 
-	fmt.Println("Server was listening on ", udpAddress)
+	fmt.Println("Server was listening on port", port)
 }
 
 func (server *Server) GetSocket() *net.UDPConn {
@@ -30,6 +33,22 @@ func (server *Server) SetSocket(socket *net.UDPConn) {
 	server.socket = socket
 }
 
+func (server *Server) GetPrudpVersion() int {
+	return server.prudpVersion
+}
+
+func (server *Server) SetPrudpVersion(prudpVersion int) {
+	server.prudpVersion = prudpVersion
+}
+
+func (server *Server) GetFragmentSize() int {
+	return server.FragmentSize
+}
+
+func (server *Server) SetFragmentSize(FragmentSize int) {
+	server.FragmentSize = FragmentSize
+}
+
 func (server *Server) GetAccessKey() string {
 	return server.accessKey
 }
@@ -38,8 +57,20 @@ func (server *Server) SetAccessKey(accessKey string) {
 	server.accessKey = accessKey
 }
 
+func (server *Server) GetKeySize() int {
+	return server.KeySize
+}
+
+func (server *Server) SetKeySize(KeySize int) {
+	server.KeySize = KeySize
+}
+
 func NewServer() *Server {
-	server := &Server{}
+	server := &Server{
+		KeySize:      32,
+		FragmentSize: 1300,
+		prudpVersion: 1,
+	}
 
 	return server
 }
